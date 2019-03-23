48b31671923d64e1c6747d5b811e83326ca060f7
Angelina Montanez <angelinamontanez80@gmail.com>
2019-03-23 22:50:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,4 +31,13 @@ end
 
 def self.find_by_name(name)
   @all.detect{ |song| song.name == name }
-end
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+  
+      
+    
