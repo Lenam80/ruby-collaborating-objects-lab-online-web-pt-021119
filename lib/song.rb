8cce0ceef45ffe18b1143a77cb79c89a2a6a68d7
Angelina Montanez <angelinamontanez80@gmail.com>
2019-03-23 22:43:07 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,34 @@
+class Song
+  attr_accessor :Song
+  @@all = []
+
+  def self.all
+    @@all
+end
+
+def save
+  self.class.all << self
+end
+
+def.self.create
+  song = self.new
+  song.save
+  song
+end
+
+def self.name_by_name(name)
+  song = self.new
+  song.name = name
+  song
+end
+
+def self.create_by_name(name)
+  song = self.new
+  song.name = name
+  song.save
+  song
+end
+
+def self.find_by_name(name)
+  @all.detect
+end
